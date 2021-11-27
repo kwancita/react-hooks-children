325286cd38b599cde9bcdc2534f787cef1d65c1a
Wasanta <kwan_u_day@hotmail.com>
2021-11-27 14:40:30 -0500

    done but not 100% clear

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,9 +8,13 @@ function Container({
   contentPosition = "center", // possible values: left, right, center
 }) {
   return (
+    //outer div refer to the parent div 
+    //the parent have been set to textPosition but not requried but with direction
+
+    //inner div refer to the childrent, set direction and contentPosition as default
     <div className={`container ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
-
+      
       <div className={`container-children ${direction} ${contentPosition}`}>
         {children}
       </div>
